da6cb57a82183a0d10e6a423f917ed1c3eee2c41
Hietzu <veeti.hietanen03@gmail.com>
2020-03-24 12:10:03 +0200

    Basic layout and some code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Kaveri_lista
+This is hopefully going to be a list that you can add and remove people from
---
+++ lista.js
@@ -0,0 +1,9 @@
+function nimilista(nimi) {
+    let nimet = '';
+}
+
+function tulostaNimet(nimet){
+    document.getElementById('nimet').innerHTML = "nimet";
+}
+
+tulostaNimet (nimet);
