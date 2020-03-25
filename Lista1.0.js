5965a9f50ca0d998476632096d4de5adfbc9968b
Hietzu <veeti.hietanen03@gmail.com>
2020-03-25 10:31:39 +0200

    1.0 Works now

---
+++ Lista1.0.js
@@ -0,0 +1,52 @@
+function myFunction () {
+    var friend1 = prompt("Please enter friend1's name", "1");
+    if (friend1 != null) {
+        document.getElementById("friend1").innerHTML =
+        "1. " + friend1;
+    }
+    var friend2 = prompt("Please enter friend2's name", "2");
+    if (friend2 != null) {
+        document.getElementById("friend2").innerHTML =
+        "2. " + friend2;
+    }
+    var friend3 = prompt("Please enter friend3's name", "3");
+    if (friend3 != null) {
+        document.getElementById("friend3").innerHTML =
+        "3. " + friend3;
+    }
+    var friend4 = prompt("Please enter friend4's name", "4");
+    if (friend4 != null) {
+        document.getElementById("friend4").innerHTML =
+        "4. " + friend4;
+    }
+    var friend5 = prompt("Please enter friend5's name", "5");
+    if (friend5 != null) {
+        document.getElementById("friend5").innerHTML =
+        "5. " + friend5;
+    }
+    var friend6 = prompt("Please enter friend6's name", "6");
+    if (friend6 != null) {
+        document.getElementById("friend6").innerHTML =
+        "6. " + friend6;
+    }
+    var friend7 = prompt("Please enter friend7's name", "7");
+    if (friend7 != null) {
+        document.getElementById("friend7").innerHTML =
+        "7. " + friend7;
+    }
+    var friend8 = prompt("Please enter friend8's name", "8");
+    if (friend8 != null) {
+        document.getElementById("friend8").innerHTML =
+        "8. " + friend8;
+    }
+    var friend9 = prompt("Please enter friend9's name", "9");
+    if (friend9 != null) {
+        document.getElementById("friend9").innerHTML =
+        "9. " + friend9;
+    }
+    var friend10 = prompt("Please enter friend10's name", "10");
+    if (friend10 != null) {
+        document.getElementById("friend10").innerHTML =
+        "10. " + friend10;
+    }
+}
